133a323276ab9727e6c676fe783d38ffbfd50c43
JUAN DAVID <juand.poveda@gmail.com>
2016-08-28 16:01:53 -0500

    Se agrega MyDaoGenerator para generar esquema de BD y DAO

---
+++ MyDaoGenerator/src/main/java/pl/surecase/eu/MyDaoGenerator.java
@@ -0,0 +1,25 @@
+package pl.surecase.eu;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+
+public class MyDaoGenerator {
+
+    public static void main(String args[]) throws Exception {
+        Schema schema = new Schema(1, "co.juandavidpoveda.pruebachuck.database");
+
+        //Frase
+        Entity frase = schema.addEntity("Frase");
+        Property fraseId = frase.addIdProperty().getProperty();
+        frase.setTableName("frase");
+        frase.addStringProperty("icon_url");
+        frase.addStringProperty("url");
+        frase.addStringProperty("value");
+
+        frase.implementsSerializable();
+
+        new DaoGenerator().generateAll(schema, args[0]);
+    }
+}
